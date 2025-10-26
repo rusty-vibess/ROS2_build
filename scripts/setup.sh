cdf970141fc4a644dfda1559e3e64f6a09007696
rusty-vibess <99050442+rusty-vibess@users.noreply.github.com>
2025-10-26 10:50:16 +0000

    chore(tidyup): Tidyup setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-# Turtle bot setup script
-source /opt/ros/humble/setup.bash
 
+# Build TB3 from src
+source /opt/ros/humble/setup.bash
 mkdir -p ~/turtlebot3_ws/src
 cd ~/turtlebot3_ws/src/
 git clone -b humble https://github.com/ROBOTIS-GIT/DynamixelSDK.git
 git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3.git
-
 cd ~/turtlebot3_ws
 colcon build --symlink-install
+# Sources required to find ROS and TB3
 echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc 
+echo "source ~/turtlebot3_ws/install/setup.bash" >> ~/.bashrc
 # Silence warnings
 echo 'export XDG_RUNTIME_DIR=/tmp/runtime-root
 mkdir -p $XDG_RUNTIME_DIR
 chmod 700 $XDG_RUNTIME_DIR' >> ~/.bashrc
-
+# Attempt to find correct NIC (if unset, find and define manually)
 echo 'export DEFAULT_IFACE="$(ip route get 1.1.1.1 | awk '\''/dev/ {print $5; exit}'\'')" ' >> ~/.bashrc
-echo "source ~/turtlebot3_ws/install/setup.bash" >> ~/.bashrc
-source ~/.bashrc
 
+source ~/.bashrc
 echo 'Setup complete...'
